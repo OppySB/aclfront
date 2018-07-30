045cac8e31b78065543c1d844c4859a1727e3c61
sbienvenu <sebastien.bienvenu@pharmagest.com>
2018-07-30 16:00:13 +0200

    agenda foolow up

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,11 +3,7 @@ import './App.css';
 import Menu from '../Menu/Menu';
 import Header from '../Header/Header';
 import Agenda from '../Agenda/Agenda';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'jquery.easing/jquery.easing.min.js';
-import 'jquery/dist/jquery.min.js';
-import 'scrollreveal/dist/scrollreveal.min.js';
-import 'magnific-popup/dist/jquery.magnific-popup.min.js';
+
 
 /**
  * Handle all the page
